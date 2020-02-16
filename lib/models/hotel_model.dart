ed1dec1840d86e2e0cc57b8d7e26da38bd40ddfe
sa-js <philipthevictor@gmail.com>
2020-02-16 22:46:19 +0500

    completed hotel view

--- lib/models/hotel_model.dart
+++ lib/models/hotel_model.dart
@@ -7,6 +7,8 @@ class Hotel {
   int rating;
   String website;
   String phone;
+  List<String> images;
+  List<String> ameneties;
 
   Hotel({
     this.imageUrl,
@@ -17,38 +19,75 @@ class Hotel {
     this.reviews,
     this.website,
     this.phone,
+    this.images,
+    this.ameneties,
   });
 }
 
 final List<Hotel> hotels = [
   Hotel(
-    imageUrl: 'assets/images/hotel0.jpg',
-    name: 'Hotel 0',
-    address: '404 Great St',
-    price: 175,
-    rating: 5,
-    website: 'https://www.radissonhotels.com/',
-    phone: '+023000034034340',
-    reviews: 1234,
-  ),
+      imageUrl: 'assets/images/hotel0.jpg',
+      name: 'Hotel 0',
+      address: '404 Great St',
+      price: 175,
+      rating: 5,
+      website: 'radissonhotels.com',
+      phone: '+023000034034',
+      reviews: 1234,
+      images: [
+        'assets/images/hotel.jpeg',
+        'assets/images/hotel0.jpg',
+        'assets/images/hotel1.jpeg',
+        'assets/images/hotel1.jpg',
+      ],
+      ameneties: [
+        'Cycle',
+        'Pool',
+        'Gym',
+        'Sports',
+        'Exchange',
+        'WiFi',
+        'Car',
+        'Guide'
+      ]),
   Hotel(
-    imageUrl: 'assets/images/hotel1.jpg',
-    name: 'Hotel 1',
-    address: '404 Great St',
-    price: 300,
-    rating: 4,
-    website: 'https://www.marriot.com/',
-    phone: '+023000034034340',
-    reviews: 134,
-  ),
+      imageUrl: 'assets/images/hotel1.jpg',
+      name: 'Hotel 1',
+      address: '404 Great St',
+      price: 300,
+      rating: 4,
+      website: 'marriot.com',
+      phone: '+023000034034',
+      reviews: 134,
+      images: [
+        'assets/images/hotel2.jpg',
+        'assets/images/hotel2.jpeg',
+        'assets/images/hotel3.jpeg',
+        'assets/images/hotel4.jpeg',
+      ],
+      ameneties: [
+        'Cycle',
+        'Pool',
+        'Gym',
+      ]),
   Hotel(
-    imageUrl: 'assets/images/hotel2.jpg',
-    name: 'Hotel 2',
-    address: '404 Great St',
-    price: 240,
-    rating: 3,
-    website: 'https://ivoryhotels.com',
-    phone: '+023000034034340',
-    reviews: 1234666,
-  ),
+      imageUrl: 'assets/images/hotel2.jpg',
+      name: 'Hotel 2',
+      address: '404 Great St',
+      price: 240,
+      rating: 3,
+      website: 'ivoryhotels.com',
+      phone: '+023000034034',
+      reviews: 1234666,
+      images: [
+        'assets/images/hotel1.jpg',
+        'assets/images/hotel0.jpg',
+        'assets/images/hotel3.jpeg',
+        'assets/images/hotel4.jpeg',
+      ],
+      ameneties: [
+        'Exchange',
+        'Cycle',
+        'WiFi',
+      ]),
 ];
--- lib/screens/hotel_screen.dart
+++ lib/screens/hotel_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_travel_ui_starter/models/activity_model.dart';
 import 'package:flutter_travel_ui_starter/models/hotel_model.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -11,59 +10,209 @@ class HotelScreen extends StatefulWidget {
 }
 
 class _HotelScreenState extends State<HotelScreen> {
+  Text _buildRatingStars(int rating) {
+    String stars = '';
+    for (int i = 0; i < rating; i++) {
+      stars += '⭐';
+    }
+    return Text(
+      stars,
+      style: TextStyle(fontSize: 10.0),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(children: <Widget>[
-      Stack(
-        children: <Widget>[
-          Container(
-            height: 150,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30.0),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black26,
-                    offset: Offset(0.0, 2.0),
-                    blurRadius: 6.0,
-                  )
-                ]),
-            child: Hero(
-              tag: widget.hotel.imageUrl,
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(15.0),
-                    bottomRight: Radius.circular(15.0)),
-                child: Image(
-                  image: AssetImage(widget.hotel.imageUrl),
-                  fit: BoxFit.cover,
+        body: Column(
+      children: <Widget>[
+        Stack(
+          children: <Widget>[
+            Container(
+              height: 150,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black26,
+                      offset: Offset(0.0, 2.0),
+                      blurRadius: 6.0,
+                    )
+                  ]),
+              child: Hero(
+                tag: widget.hotel.imageUrl,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(15.0),
+                      bottomRight: Radius.circular(15.0)),
+                  child: Image(
+                    image: AssetImage(widget.hotel.imageUrl),
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 30.0),
-            child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  IconButton(
-                      icon: Icon(Icons.close),
-                      iconSize: 30.0,
-                      color: Colors.black26,
-                      onPressed: () => Navigator.pop(context)),
-                  Row(children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 30.0),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
                     IconButton(
-                      icon: Icon(Icons.bookmark),
-                      iconSize: 30.0,
-                      color: Colors.black26,
-                      onPressed: () => print('OnPressed'),
-                    ),
-                  ])
-                ]),
+                        icon: Icon(Icons.close),
+                        iconSize: 30.0,
+                        color: Colors.black26,
+                        onPressed: () => Navigator.pop(context)),
+                    Row(children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.bookmark),
+                        iconSize: 30.0,
+                        color: Colors.black26,
+                        onPressed: () => print('OnPressed'),
+                      ),
+                    ])
+                  ]),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      IconButton(
+                          icon: Icon(Icons.location_city), onPressed: null),
+                      Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              widget.hotel.name,
+                              style: TextStyle(
+                                  color: Colors.black87,
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            Text(widget.hotel.address)
+                          ]),
+                    ],
+                  )
+                ],
+              ),
+              Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15.0),
+                  child: Row(
+                    children: <Widget>[
+                      IconButton(icon: Icon(Icons.star), onPressed: null),
+                      _buildRatingStars(widget.hotel.rating),
+                    ],
+                  )),
+            ],
           ),
-        ],
-      ),
-    ]));
+        ),
+        Container(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    IconButton(icon: Icon(Icons.web), onPressed: null),
+                    Text(widget.hotel.website)
+                  ],
+                )
+              ],
+            ),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15.0),
+                child: Row(
+                  children: <Widget>[
+                    IconButton(
+                        icon: Icon(Icons.phone_android), onPressed: null),
+                    Text(widget.hotel.phone),
+                  ],
+                )),
+          ],
+        )),
+        Container(
+            height: 100,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                // padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                itemCount: widget.hotel.images.length,
+                itemBuilder: (BuildContext context, int index) {
+                  String images = widget.hotel.images[index];
+                  return Stack(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.fromLTRB(15.0, 5.0, 10.0, 0.0),
+                        height: 80.0,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(4.0),
+                            child: Image(
+                              image: AssetImage(images),
+                              width: 100.0,
+                              fit: BoxFit.cover,
+                            )),
+                      )
+                    ],
+                  );
+                })),
+        Container(
+            height: 50,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                // padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                itemCount: widget.hotel.ameneties.length,
+                itemBuilder: (BuildContext context, int index) {
+                  String ameneties = widget.hotel.ameneties[index];
+                  return Container(
+                    margin: EdgeInsets.fromLTRB(5.0, 0.0, 5.0, 0.0),
+                    height: 80.0,
+                    child: IconButton(
+                        icon: ameneties == 'Cycle'
+                            ? Icon(FontAwesomeIcons.bicycle)
+                            : ameneties == 'Gym'
+                                ? Icon(FontAwesomeIcons.weight)
+                                : ameneties == 'Sports'
+                                    ? Icon(FontAwesomeIcons.footballBall)
+                                    : ameneties == 'Exchange'
+                                        ? Icon(FontAwesomeIcons.moneyBill)
+                                        : ameneties == 'WiFi'
+                                            ? Icon(FontAwesomeIcons.wifi)
+                                            : ameneties == 'Car'
+                                                ? Icon(FontAwesomeIcons.car)
+                                                : ameneties == 'Pool'
+                                                    ? Icon(FontAwesomeIcons
+                                                        .swimmingPool)
+                                                    : Icon(FontAwesomeIcons
+                                                        .placeOfWorship),
+                        onPressed: null),
+                  );
+                })),
+        SizedBox(
+          height: 10.0,
+        ),
+        RaisedButton(
+            onPressed: null,
+            textColor: Colors.white,
+            padding: const EdgeInsets.all(0.0),
+            child: Container(
+              color: Theme.of(context).primaryColor,
+              padding: const EdgeInsets.all(10.0),
+              child: const Text('Book Now',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  )),
+            ))
+      ],
+    ));
   }
 }
